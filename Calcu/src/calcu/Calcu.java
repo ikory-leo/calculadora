ef664f7fc6542af0a48bde4b2966a65c4f8951a8
KarenDahiana <KarenDahiana@DahianaPC>
2017-03-11 16:39:55 -0500

    calculadora 1

---
+++ Calcu/src/calcu/Calcu.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calcu;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Calcu {
+        
+    double x;
+    
+    double suma(double numeroOne,double numeroTwo){
+        
+        x = numeroOne +numeroTwo;
+        
+        return x;
+    }
+        double resta(double numeroOne,double numeroTwo){
+        
+        x = numeroOne - numeroTwo;
+        
+        return x;
+    }
+            double multiplicacion(double numeroOne,double numeroTwo){
+        
+        x = numeroOne*numeroTwo;
+        
+        return x;
+    }
+                double division(double numeroOne,double numeroTwo){
+        
+        x = numeroOne/numeroTwo;
+        
+        return x;
+    }
+    
+}
